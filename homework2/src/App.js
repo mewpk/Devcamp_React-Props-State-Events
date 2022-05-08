15453abca20ad81a50e752e73955dd8f7c0fb985
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-08 21:29:17 +0700

    save

---
+++ homework2/src/App.js
@@ -0,0 +1,13 @@
+import Container from "./Components/Container";
+import PictureCard from "./Components/PictureCard";
+
+function App() {
+  return (
+  
+  <Container>
+    <PictureCard/>
+  </Container>
+  
+  );
+ }
+export default App;
