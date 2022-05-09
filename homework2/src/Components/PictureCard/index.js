e8b34636f1c2eef19f5fc78246e0b0dd2c590384
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-09 15:42:46 +0700

    save

--- homework2/src/Components/PictureCard/index.js
+++ homework2/src/Components/PictureCard/index.js
@@ -1,54 +1,108 @@
-import React,{useState} from 'react'
-import "./Style.css"
+import React, { useState } from "react";
+import "./Style.css";
 
 export default function PictureCard() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      imgSrc: "https://picsum.photos/200/200",
+      createBy: "Mew",
+      likeCount: 1,
+      commentCount: 2,
+    },
+    {
+      id: 2,
+      imgSrc: "https://picsum.photos/200/201",
+      createBy: "Thun",
+      likeCount: 2,
+      commentCount: 3,
+    },
+    {
+      id: 3,
+      imgSrc: "https://picsum.photos/200/202",
+      createBy: "Tan",
+      likeCount: 4,
+      commentCount: 5,
+    },
+    {
+      id: 4,
+      imgSrc: "https://picsum.photos/200/203",
+      createBy: "IO",
+      likeCount: 6,
+      commentCount: 1,
+    },
+  ]);
 
-  const[like,setlike] = useState({
-   
-  }
-  )
-  const [comment,setcomment] = useState({
-    
-  })
+  const handleLikePost = (postId) => {
+    const newPosts = posts.map((post) => {
+      if (post.id === postId) {
+        post.likeCount += 1;
+      }
+      return post;
+    });
 
-  const Card = [
-    {
-      id : "01",
-      imgSrc : "https://picsum.photos/200/200",
-      createBy : "Mew",
-      likeCount : 1,
-      commentCount : 2
-    },
-    {
-      id : "02",
-      imgSrc : "https://picsum.photos/200/201",
-      createBy : "Thun",
-      likeCount : 2,
-      commentCount : 3
-    },
-    {
-      id : "03",
-      imgSrc : "https://picsum.photos/200/202",
-      createBy : "Tan",
-      likeCount : 4,
-      commentCount : 5
-    },
-    {
-      id : "04",
-      imgSrc : "https://picsum.photos/200/203",
-      createBy : "IO",
-      likeCount : 6,
-      commentCount : 1
-    }
-   
-  ]
-  const onChangeLike = (e)=>{
-    setlike = 1
-  }
-  const list = Card.map(x=><div className='grid-item'><img src={x.imgSrc} id={x.id}></img> <h3>createBy : {x.createBy}</h3><p>Like : {x.likeCount}</p><button onClick={()=>{onChangeLike()}} style={{backgroundColor : "blue",color: "white",border : 0 ,height : 30}}> Like !!</button>
-  <p>Comment : {x.commentCount}</p></div>)
+    // update stat
+    setPosts(newPosts);
+  };
 
   return (
-    <div className='PictureCard'>{list}</div>
-  )
+    <div className="Container">
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        backgroundColor: "#fafafa",
+        color: "#000",
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+        fontFamily: "sans-serif",
+        textAlign: "center",
+        padding: "0",
+        margin: "0",
+      }}
+    >
+      {posts.map((post) => (
+        <div
+          key={post.id}
+          style={{
+            border: "1px solid #000",
+            width: "200px",
+            margin: "10px",
+          }}
+        >
+          <img src={post.imgSrc} alt="post" />
+          <div>
+            <h3>{post.createBy}</h3>
+            <p>
+              <span>{post.likeCount}</span>
+              {/* <span>{post.commentCount}</span> */}
+            </p>
+          </div>
+
+          <button
+            style={{
+              backgroundColor: "#000",
+              color: "#fff",
+              padding: "0.5rem",
+              border: "none",
+              fontSize: "1rem",
+              fontWeight: "bold",
+              fontFamily: "sans-serif",
+              cursor: "pointer",
+              margin: "0.5rem",
+              borderRadius: "5px",
+            }}
+            onClick={() => {
+              handleLikePost(post.id);
+            }}
+          >
+            Like
+          </button>
+        </div>
+      ))}
+    </div>
+    </div>
+  );
 }
